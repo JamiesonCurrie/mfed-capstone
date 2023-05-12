39f2a846036b9371a57d889a32ef31988b01510e
Jamieson Currie <currie.jamieson@gmail.com>
2023-05-12 17:14:31 -0400

    * add router to App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import './App.css';
 
-import Header from "./components/Header";
-import Nav    from "./components/Nav";
-import Main   from "./components/Main";
-import Footer from "./components/Footer";
+import { Route, Routes } from "react-router-dom";
 
-
+import Header from './components/Header';
+import Main from './components/Main';
+import Nav from './components/Nav';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Header />
+      <Routes>
+        <Route index path="/*" element={<Main />} />
+      </Routes>
       <Nav id='navbar'/>
-      <Main />
       <Footer />
     </>
   );
