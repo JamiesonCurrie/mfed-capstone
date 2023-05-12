bb8133f99c1bc85c716ef7869801d431fa8be187
Jamieson Currie <currie.jamieson@gmail.com>
2023-05-12 17:21:30 -0400

    * convert Nav a to Router Links

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,29 +3,27 @@ import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars } from "@fortawesome/free-solid-svg-icons";
 
+import { Link } from 'react-router-dom';
+
 const navItems = [
-  { key: 'home',     text: 'Home'}
-, { key: 'specials', text: 'Menu'}
-, { key: 'reviews',  text: 'Reviews'}
-, { key: 'about',    text: 'About'}
-, { key: 'reserve',  text: 'Reservations'}
-, { key: 'order',    text: 'Order Online'}
-, { key: 'login',    text: 'Login'}
+  { key: 'home',     link:'',         text: 'Home'}
+, { key: 'specials', link:'specials', text: 'Menu'}
+, { key: 'reviews',  link:'reviews',  text: 'Reviews'}
+, { key: 'about',    link:'about',    text: 'About'}
+, { key: 'booking',  link:'booking',  text: 'Reservations'}
+, { key: 'order',    link:'NULL',     text: 'Order Online'}
+, { key: 'login',    link:'NULL',     text: 'Login'}
 ];
 
 const Nav = (props) => {
   const [menuClass, setMenuClass] = useState(null);
 
-  const handleLinkClick = (anchor) => () => {
+  const handleLinkClick = (e, anchor) => {
     (menuClass) ? setMenuClass(null) : setMenuClass('open');
 
-    const id = `${anchor}-section`;
-    const element = document.getElementById(id);
-    if (element) {
-      element.scrollIntoView({
-        behavior: "smooth",
-        block: "start",
-      });
+    if (anchor === 'NULL') {
+      e.preventDefault();
+      alert("Coming Soon!");
     }
   };
 
@@ -38,12 +36,14 @@ const Nav = (props) => {
       <FontAwesomeIcon id={props.id + '-burgernav'} icon={faBars} size='2xl' onClick={handleBurgerClick} />
       <ul className={menuClass}>
         {navItems.map((navItem) => {
-          const itemKey = navItem.key;
           return (
-            <li key={itemKey}>
-              <a href={'#' + itemKey} onClick={handleLinkClick(itemKey)}>
+            <li key={navItem.key}>
+              <Link
+                to={'/' + navItem.link}
+                onClick={(e) => handleLinkClick(e, navItem.link)}
+              >
                 {navItem.text}
-              </a>
+              </Link>
             </li>
           );
         })}
