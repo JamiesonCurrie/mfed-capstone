835077aa0a49f8c0fe789e2b215469346ccd17ed
Jamieson Currie <currie.jamieson@gmail.com>
2023-05-13 15:39:45 -0400

    * remove strict mode for data api

--- src/index.js
+++ src/index.js
@@ -6,12 +6,15 @@ import reportWebVitals from './reportWebVitals';
 import {BrowserRouter as Router} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
+//<React.StrictMode>
+//</React.StrictMode>
 root.render(
-  <React.StrictMode>
+ 
     <Router>
       <App />
     </Router>
-  </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
